c47e1d7ca1e9e754b2c25513b815a88d83a2b456
Mario Garza <mario.garza02@utrgv.edu>
2020-04-24 04:07:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    binding.pry
   end
 end
 end
